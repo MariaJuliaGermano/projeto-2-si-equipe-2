808e0cbc3d25774cae1ddf0e52d2ba44eb06ac01
rvjq <rvjq@cesar.school>
2024-10-31 20:37:04 -0300

    Adicionado resposta do cliente com os dados do teste para o servidor.

--- mbti/teste_mbti/models.py
+++ mbti/teste_mbti/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.conf import settings
+import os, json
 
-# Create your models here.
+data = open(os.path.join(settings.BASE_DIR,"teste_mbti\\json\\simple_mbti_questions.json"), "r", encoding= 'utf-8')
+data = json.load(data)
--- mbti/teste_mbti/views.py
+++ mbti/teste_mbti/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from django.conf import settings
-import json
-import os
+import json, os
 
 def home(request):
     if request.method == "GET":
         data = open(os.path.join(settings.BASE_DIR,"teste_mbti\\json\\simple_mbti_questions.json"), "r", encoding= 'utf-8')
         data = json.load(data)
-        print(data)
         context = {'data':data}
         return render(request, 'teste_mbti/testepersonalidade.html', context=context)
     elif request.method == "POST":
-        return 0
+        receivedData = request.POST["data"]
+        print("Incoming info: " + receivedData)
+        return HttpResponse(status=200)
