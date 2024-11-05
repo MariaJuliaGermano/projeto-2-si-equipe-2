ce541252677278ab6c7b0c552dc6e8a74d66c615
rbvfn profissional <rbvfn.profissional@gmail.com>
2024-11-04 21:08:14 -0300

    cadastro no banco de dados das respostas do formulário do teste de personalidade

--- mbti/teste_mbti/models.py
+++ mbti/teste_mbti/models.py
@@ -4,3 +4,14 @@ import os, json
 
 data = open(os.path.join(settings.BASE_DIR,"teste_mbti\\json\\simple_mbti_questions.json"), "r", encoding= 'utf-8')
 data = json.load(data)
+
+class respostas(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    pergunta_1 = models.TextField()
+    pergunta_2 = models.TextField()
+    pergunta_3 = models.TextField()
+    pergunta_4 = models.TextField()
+
+    def __str__(self):
+        return "Resposta"
--- mbti/teste_mbti/views.py
+++ mbti/teste_mbti/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.conf import settings
 import json, os
+from .models import respostas
 
 def home(request):
     if request.method == "GET":
@@ -11,7 +12,30 @@ def home(request):
     elif request.method == "POST":
         receivedData = request.POST["data"]
         receivedData = receivedData.split(",")
-        print("Incoming info: " + receivedData[1])
+
+        pergunta_1 = ""
+        pergunta_2 = ""
+        pergunta_3 = ""
+        pergunta_4 = ""
+        tabela = respostas()
+
+        for n in range(0, 11):
+            pergunta_1 += receivedData[n]
+        for n in range(11, 22):
+            pergunta_2 += receivedData[n]
+        for n in range(22, 35):
+            pergunta_3 += receivedData[n]
+        for n in range(35, 48):
+            pergunta_4 += receivedData[n]
+
+        tabela.pergunta_1 = pergunta_1[:]
+        tabela.pergunta_2 = pergunta_2[:]
+        tabela.pergunta_3 = pergunta_3[:]
+        tabela.pergunta_4 = pergunta_4[:]
+
+        tabela.save() 
+
+        #print("Incoming info: " + receivedData[:])
         return redirect('results')
 
 def results(request):
