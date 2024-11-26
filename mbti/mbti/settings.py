51eea7527a6b5dd12d8e80e4924bc51fb821ab82
Terry Damasio <terrydamasio.dev@gmail.com>
2024-11-26 01:48:46 -0300

    ajuste

--- mbti/mbti/settings.py
+++ mbti/mbti/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-njemouz3%*7rgcn=b+prm$@w_id74+g7=hd(553h_@z49jt$gu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
