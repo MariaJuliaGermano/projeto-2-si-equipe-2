34659f63f7090d26430cfcdb484328f0eeaf6546
Terry Damasio <133397381+terrydamasio@users.noreply.github.com>
2024-11-26 01:03:53 -0300

    Revert "Revert 14 back end sprint4"

---
+++ mbti/teste_mbti/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('home/', views.home, name='home'),
+    path('results/', views.results, name='results'),
+    path('equipe/', views.equipe, name='equipe'),
+    path('tiposPersonalidades', views.tiposPersonalidades, name='tipos'),
+    path('feedback/', views.feedback, name='feedback'),
+]
