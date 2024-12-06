2e3a614b497ca717421e5911454c92beba40b400
rbvfn profissional <rbvfn.profissional@gmail.com>
2024-12-06 00:36:06 -0300

    Upgrade: importação de perfis por meio de um arquivo excel

--- mbti/auth_mbti/urls.py
+++ mbti/auth_mbti/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('cadastro', views.cadastrar, name='cadastro'),
     path('login', views.login, name='login'),
     path('logout', views.logout, name='logout'),
+    path('cadExel', views.cadExel, name='cadExcel'),
+    # path('confirmacao', views.confirmacao, name='confirmacao')
 ]
--- mbti/teste_mbti/admin.py
+++ mbti/teste_mbti/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import respostas
+from auth_mbti.models import arquivosExel
 
 admin.site.register(respostas)
+admin.site.register(arquivosExel)
 
 # Register your models here.
